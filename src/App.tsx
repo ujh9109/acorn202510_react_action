db6be83b086a74f35b673baab2169c0227c942f7
yujaehwang <ujh9109@gmail.com>
2025-10-29 11:44:03 +0900

    제목을 수정함

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>제목을 수정</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
